18eeeb328a303b4ee321c37b6f59d815218f22bc
Oskar Puchala <oskar.puchalaa@gmail.com>
2022-01-04 22:27:53 +0100

    Version 3

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Oskar";
+        int myAge = 24;
+        double height = 1.89;
+        char carClass = 'P';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(carClass);
+        System.out.println(isGoodProgrammer);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String mySchool = "UJ";
+        int myYear = 1997;
+        double myRating = 4.55;
+        char highestGrade = 'A';
+        boolean justStarted = true;
+        char lowestGrade = 'F';
+        System.out.println(mySchool);
+        System.out.println(myYear);
+        System.out.println(myRating);
+        System.out.println(highestGrade);
+        System.out.println(justStarted);
+        System.out.println(lowestGrade);
+    }
+}
