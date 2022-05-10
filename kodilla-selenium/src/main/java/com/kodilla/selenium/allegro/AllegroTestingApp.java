009e5c69d96a9fb7500a590e4324c5385d8fae6b
DeOscarr <97104580+DeOscarr@users.noreply.github.com>
2022-05-10 14:36:24 +0200

    Initial version

--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingApp.java
@@ -4,8 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class AllegroTestingApp {
     public static void main(String[] args) {
@@ -24,5 +27,10 @@ public class AllegroTestingApp {
         WebElement searchField = driver.findElement(By.xpath("//*[@class=\"mpof_ki mqu1_21 mp4t_0 m3h2_0 mryx_0 munh_0 mgn2_14 mp0t_0a mgmw_wo mli8_k4 _535b5_3gmLS mp7g_oh\"]/input"));
         searchField.sendKeys("Mavic mini");
         searchField.submit();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("section>article")));
+
+        List<WebElement> elements = driver.findElements(By.cssSelector("section>article"));
+        System.out.println(elements.get(0).getText());
     }
 }
